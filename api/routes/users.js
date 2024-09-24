c47a0f027fc39d84b972f5898f1ddb2b020c8036
github_aliyukabir1 <aliyukabirlawal@gmail.com>
2024-09-24 10:34:53 +0100

    Feature: Reset Password endpoint.

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/users');
 router.get('/',userController.get_users)
 router.post('/signup',userController.sign_up)
 router.post('/signin',userController.sign_in)
+router.put('/reset',userController.reset_password)
 
 module.exports = router;
